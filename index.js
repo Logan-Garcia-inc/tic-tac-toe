a47f11a643ddb0b4ddb1c9b13dcb21225bcef68e
Logan <92747680+celestial-bean@users.noreply.github.com>
2023-08-05 09:43:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-let winner=false
+var winner=false;
 function restart(){
 for (var i=0; i<=9; i++){
-board.data[`s${i}`]=""
-document.querySelector(".result").textContent="result:"
-board.updateDisplay()
-winner=0
-board.turn=true
+board.data[`s${i}`]="";
+document.querySelector(".result").textContent="result:";
+board.updateDisplay();
+winner=0;
+board.turn=true;
 }
 }
 function innitSpots(){
-for (let i = 1; i < 10; i++) {
-  let target = document.getElementsByClassName(`s${i}`)[0]
+for (var i = 1; i < 10; i++) {
+  var target = document.getElementsByClassName(`s${i}`)[0];
   target.addEventListener("click", function(e) {
   if (!winner){
-    let dataPosition = board.data[`${e.target.classList[1]}`]
+    var dataPosition = board.data[`${e.target.classList[1]}`];
     
     if (!dataPosition) {
       board.data[`${e.target.classList[1]}`] = playTurn();
-      board.checkForWinner()
+      board.checkForWinner();
     }
     
-    console.log(board.data)
+    console.log(board.data);
 
-  }})
+  }});
 }}
 function playTurn() {
   if (board.turn) {
-    return player1.team
+    return player1.team;
 
   } else {
-    return player2.team
+    return player2.team;
   }
 
 }
@@ -37,8 +37,8 @@ const player = (name, team) => {
   return {
     name,
     team,
-  }
-}
+  };
+};
 const player1 = player("player1", "X");
 const player2 = player("player2", "O");
 
@@ -46,51 +46,51 @@ const player2 = player("player2", "O");
 const Board = () => {
 const updateDisplay = () => {
     for (var i = 1; i <= 9; i++) {
-      document.querySelector(`.s${i}`).textContent = data[`s${i}`]
+      document.querySelector(`.s${i}`).textContent = data[`s${i}`];
     }
 
   }
   function win() {
     if (turn) {
-      console.log("player 1 wins")
-      document.querySelector(".result").textContent = "Player 1 wins."
+      console.log("player 1 wins");
+      document.querySelector(".result").textContent = "Player 1 wins.";
     } else {
-      console.log("player 2 wins")
-      document.querySelector(".result").textContent = "Player 2 wins."
+      console.log("player 2 wins");
+      document.querySelector(".result").textContent = "Player 2 wins.";
     }
-    winner=true
+    winner=true;
  }
   const checkForWinner = () => {
-  updateDisplay()
+  updateDisplay();
     if (data["s1"] == data["s2"] && data["s2"] == data["s3"] && data["s3"] !== "") {
-      win()
+      win();
     }
     if (data["s4"] == data["s5"] && data["s5"] == data["s6"] && data["s6"] !== "") {
-      win()
+      win();
     }
     if (data["s7"] == data["s8"] && data["s8"] == data["s9"] && data["s9"] !== "") {
-      win()
+      win();
     }
     if (data["s1"] == data["s4"] && data["s4"] == data["s7"] && data["s7"] !== "") {
-      win()
+      win();
     }
     if (data["s2"] == data["s5"] && data["s5"] == data["s8"] && data["s8"] !== "") {
-      win()
+      win();
     }
     if (data["s3"] == data["s6"] && data["s6"] == data["s9"] && data["s9"] !== "") {
-      win()
+      win();
     }
     if (data["s1"] == data["s5"] && data["s5"] == data["s9"] && data["s9"] !== "") {
-      win()
+      win();
     }
     if (data["s3"] == data["s5"] && data["s5"] == data["s7"] && data["s7"] !== "") {
-      win()
+      win();
     }
 board.turn = !board.turn
   }
   
-  let turn = true;
-  let data = {
+  var turn = true;
+  var data = {
     s1: "",
     s2: "",
     s3: "",
@@ -100,7 +100,7 @@ board.turn = !board.turn
     s7: "",
     s8: "",
     s9: ""
-  }
+  };
 
 
   return {
@@ -108,12 +108,12 @@ board.turn = !board.turn
     turn,
     updateDisplay,
     checkForWinner
-  }
+  };
 }
-const board = Board()
+const board = Board();
 
 
 
 // init spots
 
-innitSpots()
+innitSpots();
